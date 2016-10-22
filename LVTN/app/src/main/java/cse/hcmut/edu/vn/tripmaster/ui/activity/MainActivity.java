e0ee6b82faa32aaff720733aee3d1251f45ddc46
DanhVT <danhvt@cardano-labo.com>
2016-10-22 20:39:31 +0700

    conclusion 1 tab

---
+++ LVTN/app/src/main/java/cse/hcmut/edu/vn/tripmaster/ui/activity/MainActivity.java
@@ -0,0 +1,83 @@
+package cse.hcmut.edu.vn.tripmaster.ui.activity;
+
+import android.graphics.PorterDuff;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import cse.hcmut.edu.vn.tripmaster.R;
+import cse.hcmut.edu.vn.tripmaster.ui.fragment.MediaFragment;
+import cse.hcmut.edu.vn.tripmaster.ui.fragment.MyTripFragment;
+import cse.hcmut.edu.vn.tripmaster.ui.fragment.TripFragment;
+import cse.hcmut.edu.vn.tripmaster.ui.fragment.UserFragment;
+import cse.hcmut.edu.vn.tripmaster.ui.widget.ViewPagerAdapter;
+
+import static android.graphics.Color.parseColor;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private int[] tabIcons = {
+            R.drawable.ic_home_black_24dp,
+            R.drawable.ic_map_black_24dp,
+            R.drawable.ic_video_library_black_24dp,
+            R.drawable.ic_person_black_24dp
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TripFragment(), "ONE");
+        adapter.addFragment(new MyTripFragment(), "TWO");
+        adapter.addFragment(new MediaFragment(), "THREE");
+        adapter.addFragment(new UserFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+
+        tabLayout.getTabAt(0).getIcon().setColorFilter(parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(parseColor("#000000"), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+}
---
+++ LVTN/app/src/main/java/cse/hcmut/edu/vn/tripmaster/ui/fragment/TripFragment.java
@@ -0,0 +1,10 @@
+package cse.hcmut.edu.vn.tripmaster.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by AnTD on 10/22/2016.
+ */
+
+public class TripFragment extends Fragment {
+}
