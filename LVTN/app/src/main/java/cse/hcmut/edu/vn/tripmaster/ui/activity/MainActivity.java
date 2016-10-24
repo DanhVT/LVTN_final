549b34abeb329bb76ae20df4696b80962d870a60
DanhVT <danhvt@cardano-labo.com>
2016-10-24 12:06:01 +0700

    Update

--- LVTN/app/src/main/java/cse/hcmut/edu/vn/tripmaster/ui/activity/MainActivity.java
+++ LVTN/app/src/main/java/cse/hcmut/edu/vn/tripmaster/ui/activity/MainActivity.java
@@ -97,9 +97,6 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
