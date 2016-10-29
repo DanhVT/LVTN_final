7801112d993fc00c2558b19b192632dbbcf5762d
DanhVT <danhvt@cardano-labo.com>
2016-10-29 11:40:32 +0700

    Service server request

---
+++ LVTN/app/src/main/java/cse/hcmut/edu/vn/tripmaster/service/http/ServerRequest.java
@@ -0,0 +1,42 @@
+package cse.hcmut.edu.vn.tripmaster.service.http;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by AnTD on 10/29/2016.
+ */
+
+public class ServerRequest {
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    public ServerRequest(){
+    }
+
+    private String get(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    private String post(String url, String json) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
